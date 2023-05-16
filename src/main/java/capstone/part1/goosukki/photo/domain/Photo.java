b6fd0f4f037614342cac8adcb1b04ddb8678c632
00blowup <00blowup@gmail.com>
2023-05-16 17:18:32 +0900

    feat : Photo 도메인에 캡션 필드 추가

--- src/main/java/capstone/part1/goosukki/photo/domain/Photo.java
+++ src/main/java/capstone/part1/goosukki/photo/domain/Photo.java
@@ -13,13 +13,25 @@ public class Photo {
     private String sequence;
     // 파일명
     private String filename;
+    // 캡션
+    private String caption;
 
     public Photo (Long sequence, String filename) {
         this.sequence = sequence.toString();
         this.filename = filename;
+        this.caption = null;
     }
+    public Photo (Long sequence, String filename, String caption) {
+        this.sequence = sequence.toString();
+        this.filename = filename;
+        this.caption = caption;
+    }
+
 
     public void setSequence(String sequence) {
         this.sequence = sequence;
+    }
+    public void setCaption(String caption) {
+        this.caption = caption;
     }
 }
